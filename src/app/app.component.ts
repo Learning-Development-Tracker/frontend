c565e97523a265e7bd609912ee17cd4a460103ab
Spencer Castro <82010603@corp.root>
2024-04-04 19:17:14 +0800

    setup api service for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminComponent } from './views/admin/admin.component';
+import { UserComponent } from './views/user/user.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminComponent],
+  imports: [RouterOutlet, AdminComponent, UserComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

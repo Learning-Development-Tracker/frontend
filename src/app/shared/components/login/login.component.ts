123123c8ac8e2a71bb70409cca533f0120ef4201
bryan_borja <borjabryanjay@gmail.com>
2024-04-17 09:51:45 +0800

    login form modification v2

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { CustomBottonComponent } from '../custom-button/custom-button.component';
+import { ModalService } from '../modal/modal.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ModalOptions } from '../modal/modal-options';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +13,31 @@ import { CommonModule } from '@angular/common';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('vcrPass', { static: true, read: ViewContainerRef })
+  vcrPassword!: ViewContainerRef;
+  @ViewChild('vcrReset', { static: true, read: ViewContainerRef })
+  vcrReset!: ViewContainerRef;
+  @ViewChild('vcrCreate', { static: true, read: ViewContainerRef })
+  vcrCreate!: ViewContainerRef;
+  @ViewChild('vcrUpdate', { static: true, read: ViewContainerRef })
+  vcrUpdate!: ViewContainerRef;
   @Output() emailChanged = new EventEmitter<string>();
   @Output() passwordChanged = new EventEmitter<string>();
   @Output() usernameChanged = new EventEmitter<string>();
   @Output() loginButtonClicked = new EventEmitter<void>();
   @Input() errMessage: string | undefined;
-  buttonColor: string = "var(--black)";
-  width: string = "90%";
-  visible: boolean = true;  
+  buttonColor: string = "var(--dark-blue)";
+  buttonWidth: string = "85%";
+  buttonMargin: string = "5px 4.5%";
+  buttonSize: string = "40px";
+  visible: boolean = true;
+  visibleConfirm: boolean = true;
+  forgotPassword: string = '';
+  resetPassword: string = 'resetPW';
+  createPassword: string = 'newPW';
+  updatePassword: string = 'updPW';
+
+  constructor(private modalService: ModalService) { }
 
   onEmailChange(email: Event) {
     const target = email.target as HTMLInputElement;
@@ -36,7 +55,7 @@ export class LoginComponent {
     this.usernameChanged.emit(target.value);
   }
 
-  loginClick(){
+  loginClick() {
     this.loginButtonClicked.emit();
   }
 
@@ -45,11 +64,85 @@ export class LoginComponent {
   }
 
   changeVisibility(): string {
-    return this.visible ?  'fa-regular fa-eye-slash': 'fa-regular fa-eye';
+    return this.visible ? 'fa-regular fa-eye-slash' : 'fa-regular fa-eye';
   }
 
   changeType(): string {
     return this.visible ? 'password' : 'text';
   }
 
+  showPWConfirm() {
+    this.visibleConfirm = !this.visibleConfirm;
+  }
+
+  changeVisibilityConfirm(): string {
+    return this.visibleConfirm ? 'fa-regular fa-eye-slash' : 'fa-regular fa-eye';
+  }
+
+  changeTypeConfirm(): string {
+    return this.visibleConfirm ? 'password' : 'text';
+  }
+
+  openModalTemplate1(view: TemplateRef<Element>) {
+    this.modalService.open(this.vcrPassword, view, {
+      animations: {
+        modal: {
+          enter: 'enter-slide-down 0.8s',
+        },
+        overlay: {
+          enter: 'fade-in 0.8s',
+          leave: 'fade-out 0.3s forwards',
+        },
+      },
+      size: {
+        width: '14rem',
+      },
+    });
+  }
+
+ options:ModalOptions = {
+  animations: {
+    modal: {
+      enter: 'enter-slide-down 0.8s',
+    },
+    overlay: {
+      enter: 'fade-in 0.8s',
+      leave: 'fade-out 0.3s forwards',
+    },
+  },
+  size: {
+    width: '24rem',
+  }
+}
+
+openModalTemplate2(view: TemplateRef<Element>) {
+  this.modalService.open(this.vcrReset, view, this.options);
+}
+
+openModalTemplate3(view: TemplateRef<Element>) {
+  this.modalService.open(this.vcrCreate, view, this.options);
+}
+
+openModalTemplate4(view: TemplateRef<Element>) {
+  this.modalService.open(this.vcrUpdate, view, this.options);
+}
+
+close() {
+  this.modalService.close();
+}
+
+resetPass() {
+  this.forgotPassword = this.resetPassword;
+  this.close();
+}
+
+createPass() {
+  this.forgotPassword = this.createPassword;
+  this.close();
+}
+
+updatePass() {
+  this.forgotPassword = this.updatePassword;
+  this.close();
+}
 }

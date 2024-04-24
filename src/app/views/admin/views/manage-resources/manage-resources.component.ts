11032c4b020b508e3c48d65359ab9765cb081332
Spence <spencercastro15@gmail.com>
2024-04-25 02:00:02 +0800

    sample demo for tabs

--- src/app/views/admin/views/manage-resources/manage-resources.component.ts
+++ src/app/views/admin/views/manage-resources/manage-resources.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-manage-resources',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './manage-resources.component.html',
   styleUrl: './manage-resources.component.css'
 })
 export class ManageResourcesComponent {
+  public activeTab: string = 'Certification';
+  constructor() {}
 
+  onChangeTab(type: string) {
+    switch(type) {
+      case 'Training':
+        return this.activeTab = 'Training'
+      case 'Reports':
+        return this.activeTab = 'Reports'
+      default:
+        return this.activeTab = 'Certification'
+    }
+  }
 }

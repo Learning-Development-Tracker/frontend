c636bccd8dfd1f22367635dec49a1068d86397fb
bryan_borja <borjabryanjay@gmail.com>
2024-04-25 11:37:00 +0800

    certification ui screen

---
+++ src/app/shared/components/temp-tab-slider/temp-tab-slider.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CertificationComponent } from '../../../views/certification/certification.component';
+
+@Component({
+  selector: 'temp-tab-slider',
+  standalone: true,
+  imports: [CertificationComponent],
+  templateUrl: './temp-tab-slider.component.html',
+  styleUrl: './temp-tab-slider.component.css'
+})
+export class TempTabSliderComponent {
+
+}

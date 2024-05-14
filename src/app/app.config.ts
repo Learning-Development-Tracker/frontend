1731418f6fe819b00fe34a1c239935f4318b009f
bryan_borja <78636729+bborja-ext@users.noreply.github.com>
2024-05-14 14:35:59 +0800

    login authentication security token
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/app/app.component.html
    #	src/app/app.component.ts
    #	src/app/app.config.ts
    #	src/app/app.routes.ts
    #	src/app/views/admin/admin.component.ts
    #	src/styles.css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+﻿import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { LoginInterceptor } from './shared/components/login/login.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), 
+    provideHttpClient(), 
+    { provide: HTTP_INTERCEPTORS,
+      useClass: LoginInterceptor,
+      multi: true
+    },
+    importProvidersFrom([BrowserAnimationsModule])
+  ]
 };

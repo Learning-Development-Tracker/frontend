e9e33c9482eed9493a76d227a5f71a4d9e813bd5
bryan_borja <78636729+bborja-ext@users.noreply.github.com>
2024-05-16 22:07:52 +0800

    header and token interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
 ﻿import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { LoginInterceptor } from './shared/components/login/login.interceptor';
+import { HeaderInterceptor } from './shared/components/login/header.interceptor';
+import { TokenInterceptor } from './shared/components/login/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), 
     provideHttpClient(), 
     { provide: HTTP_INTERCEPTORS,
-      useClass: LoginInterceptor,
+      useClass: HeaderInterceptor,
       multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
     },
     importProvidersFrom([BrowserAnimationsModule])
   ]

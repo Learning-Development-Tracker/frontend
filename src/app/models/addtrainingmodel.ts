de446e93d040b929f56b75f6d28a5742389e880f
Spencer Castro <82010603@corp.root>
2024-05-20 12:22:38 +0800

    from develop, add edit trainigs, add more training popup, missing codes

---
+++ src/app/models/addtrainingmodel.ts
@@ -0,0 +1,26 @@
+export class AddTrainingModel {
+    id?: string;
+    trainingName?: string;
+    trainingType?: string;
+    productName?: string;
+    startDate?: Date;
+    dueDate?: Date;
+    preReq?: string;
+    desc?: string;
+    trLink?: string;
+    role?: string;
+    required?: boolean;
+    hasCert?: boolean;
+    certName?: string;
+    duration?: string;
+    fees?: number;
+  //   currency?: string;
+    certLink?: string;
+    radio1?: boolean;
+    radio1value?: number;
+    radio2?: boolean;
+    radio2value?: number;
+    radio3?: boolean;
+    trCondition?: number;
+    trConditionValue?: number;
+}

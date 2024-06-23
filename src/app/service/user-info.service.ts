c83e0499e0a50890fddf3a5cf2ffeb4a35993c74
Ervin Joshua Santos <ervinjoshua.sj.santos@pccw.com>
2024-06-24 02:23:42 +0800

    Cert List and View Cert FE

--- src/app/service/user-info.service.ts
+++ src/app/service/user-info.service.ts
@@ -6,14 +6,14 @@ import { ConfigService } from '../authentication/config.services';
   providedIn: 'root'
 })
 export class UserInfoService {
-    private baseUrl = '/api/v1/users'; // Replace with your actual backend API URL
+    private baseUrl = '/api/v1/resources'; // Replace with your actual backend API URL
 
   
     httpClient = inject(HttpClient);
     constructor(private configService: ConfigService) {}
 
     getProfileInformation(id: string){
-        return this.httpClient.get(this.configService.apiUrl + this.baseUrl + "/get-profile-info/"+ id);
+        return this.httpClient.get(this.configService.apiUrl + this.baseUrl + "/getProfileInfo/"+ id);
     }
 
     getUserRoles(){
